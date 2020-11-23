d5177dd7f1d469a5ad006139d9b9d70ad5608abe
Calvin-Davidson <calvin2003davidson@gmail.com>
2020-11-23 13:53:52 +0100

    changed the player characther

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float jumpHeight = 4.5f;
     void Start()
     {
         _rigidbody2D = this.GetComponent<Rigidbody2D>();
@@ -13,17 +15,19 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
+            this.transform.localScale = new Vector3(-Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
         
         if (Input.GetKey(KeyCode.D))
         {
             this.transform.position += new Vector3(3 * Time.deltaTime, 0, 0);
+            this.transform.localScale = new Vector3(Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Vector3 currentVel = _rigidbody2D.velocity;
-            currentVel.y += 4.5f;
+            currentVel.y += jumpHeight;
             _rigidbody2D.velocity = currentVel;
         }
     }
