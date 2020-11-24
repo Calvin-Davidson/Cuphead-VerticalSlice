f26eb34bea571de105965a16959b15bf7b7f7c4a
Calvin-Davidson <calvin2003davidson@gmail.com>
2020-11-24 10:19:00 +0100

    Camera movement system

--- Assets/Scripts/Camera/CameraFollowPath.cs
+++ Assets/Scripts/Camera/CameraFollowPath.cs
@@ -14,7 +14,7 @@ public class CameraFollowPath : MonoBehaviour
 
     [SerializeField] private GameObject debugObjPrevPos;
     [SerializeField] private GameObject debugObjNextPos;
-    
+
     private void Awake()
     {
         _locations = new List<Vector3>();
@@ -23,6 +23,7 @@ public class CameraFollowPath : MonoBehaviour
         {
             _locations.Add(transform1.position);
         }
+
         _locations.Remove(_locations[0]);
 
         var trans = transform.position;
@@ -35,61 +36,83 @@ public class CameraFollowPath : MonoBehaviour
 
     public void Update()
     {
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            return;
+        }
+
         CheckValidLocationsCounters();
         debugObjPrevPos.transform.position = _locations[_previousLocation];
         debugObjNextPos.transform.position = _locations[_nextLocation];
-        
+
         Vector3 startingPos = transform.position;
         Vector3 newPos = startingPos;
-        
+
         if (Input.GetKey(KeyCode.D))
         {
             newPos = GetCameraPosition(_locations[_nextLocation]);
 
             float dist = Vector3.Distance(transform.position, _locations[_nextLocation]);
-            if (dist < 0.015)
+            if (dist < 0.05)
             {
                 _nextLocation += 1;
                 _previousLocation += 1;
                 CheckValidLocationsCounters();
-            } 
+            }
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             newPos = GetCameraPosition(_locations[_previousLocation]);
             float dist = Vector3.Distance(transform.position, _locations[_previousLocation]);
-            if (dist < 0.015)
+            if (dist < 0.05)
             {
                 _nextLocation -= 1;
                 _previousLocation -= 1;
                 CheckValidLocationsCounters();
-            } 
+            }
         }
+
         transform.position = newPos;
     }
 
-    public Vector3 GetCameraPosition(Vector3 targetPos)
+    private Vector3 GetCameraPosition(Vector3 targetPos)
     {
         Vector3 startingPos = transform.position;
         var target = targetPos;
         target.z = startingPos.z;
-        target.y = Vector3.MoveTowards(startingPos, target, 3 * Time.deltaTime).y;
+        // 4.102742985f
+        //target.y = Vector3.MoveTowards(startingPos, target, CalculateCameraSpeed() * Time.deltaTime).y;
+        target.y = Mathf.Lerp(_locations[_previousLocation].y, _locations[_nextLocation].y, CalculateCameraSpeed());
         target.x = playerTransform.position.x;
         return target;
     }
 
-    public void CheckValidLocationsCounters()
+    private float CalculateCameraSpeed()
     {
-        if (_nextLocation > _locations.Count)
+        float diffX = Math.Abs(_locations[_previousLocation].x - _locations[_nextLocation].x);
+        float diffY = Math.Abs(_locations[_previousLocation].y - _locations[_nextLocation].y);
+
+        float minX = Mathf.Min(_locations[_previousLocation].x, _locations[_nextLocation].x);
+        float maxX = Mathf.Max(_locations[_previousLocation].x, _locations[_nextLocation].x);
+        return (transform.position.x-minX)/(maxX-minX);
+        
+//       return Mathf.Lerp(_locations[_previousLocation].y, _locations[_nextLocation].y, 4.5f);
+//        return (float) Math.Sqrt((float) Math.Pow(diffX, 2) + (float) Math.Pow(diffY, 2));
+    }
+
+    private void CheckValidLocationsCounters()
+    {
+        if (_nextLocation >= _locations.Count)
         {
             _previousLocation = _locations.Count - 2;
             _nextLocation = _locations.Count - 1;
         }
+
         if (_previousLocation < 0)
         {
             _previousLocation = 0;
             _nextLocation = 1;
         }
-
     }
 }
