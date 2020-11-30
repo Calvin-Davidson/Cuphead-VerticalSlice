736bac2225fe66e75bf617f1995f581762912ee3
Calvin-Davidson <calvin2003davidson@gmail.com>
2020-11-30 13:46:55 +0100

    fixed walk animation, and cleaned up camera code

--- Assets/Scripts/Camera/CameraFollowPath.cs
+++ Assets/Scripts/Camera/CameraFollowPath.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class CameraFollowPath : MonoBehaviour
 {
-    [SerializeField] private Transform playerTransform; //Start transform of the player
-    [SerializeField] private GameObject parentObj; //Empty object reference. All children of this object will be used for the path following
+    [SerializeField] private Transform playerTransform;
+
+    [SerializeField]
+    private GameObject
+        parentObj; //Empty object reference. All children of this object will be used for the path following
+
+    [SerializeField] private float tolerance = 0.1f;
     private List<Vector3> _locations; //Positions that the camera y will use
     private int _previousLocation = 0; //Index of start location first point
     private int _nextLocation = 1; //Index of start location second point
@@ -14,17 +19,21 @@ public class CameraFollowPath : MonoBehaviour
     //Debug visualization of the 2 points the camera is bound by in the editor
     [SerializeField] private GameObject debugObjPrevPos;
     [SerializeField] private GameObject debugObjNextPos;
+
     private void Awake()
     {
         _locations = new List<Vector3>(); //Initialize position array
-        Transform[] transforms = parentObj.GetComponentsInChildren<Transform>(); //Get all child empty objects that have a transform from the parent object
+        Transform[]
+            transforms =
+                parentObj
+                    .GetComponentsInChildren<Transform>(); //Get all child empty objects that have a transform from the parent object
         foreach (var childTransform in transforms)
         {
             _locations.Add(childTransform.position); //Add child transform.position to the position array
         }
 
         _locations.Remove(_locations[0]);
-        
+
         //Initial movement
         var currentTransform = transform.position; //Get current transform
         var targetPos = _locations[_nextLocation]; //Get target position
@@ -36,41 +45,31 @@ public class CameraFollowPath : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) //Do nothing if both left and right are pressed at the same time
+        int axis = (int) Input.GetAxisRaw("Horizontal");
+        int requiredLocation;
+
+        switch (axis)
         {
-            return;
+            case 1:
+                requiredLocation = _nextLocation;
+                break;
+            case -1:
+                requiredLocation = _previousLocation;
+                break;
+            default:
+                return;
         }
 
-        CheckValidLocationsCounters(); //Double check whether the indexes calculated last frame are still valid
-        debugObjPrevPos.transform.position = _locations[_previousLocation]; //Show debug objects in the editor
-        debugObjNextPos.transform.position = _locations[_nextLocation];
-        Vector3 targetPos = transform.position; //Get current position
-
-        if (Input.GetKey(KeyCode.D)) //Move right
+        Vector3 newPosition = GetCameraPosition(_locations[requiredLocation]);
+        float distanceToPreviousPoint = Vector3.Distance(transform.position, _locations[requiredLocation]);
+        if (distanceToPreviousPoint < tolerance)
         {
-            targetPos = GetCameraPosition(_locations[_nextLocation]); //Update target position based on next location
-
-            float distanceToNextPoint = Vector3.Distance(transform.position, _locations[_nextLocation]); //Check the distance between the camera and the next point
-            if (distanceToNextPoint < 0.05) //If distance is small, go to the next set of points
-            {
-                _nextLocation += 1;
-                _previousLocation += 1;
-            }
+            _nextLocation += axis;
+            _previousLocation += axis;
         }
 
-        if (Input.GetKey(KeyCode.A)) //Move left
-        {
-            targetPos = GetCameraPosition(_locations[_previousLocation]); //Update target position based on previous location
-            float distanceToPreviousPoint = Vector3.Distance(transform.position, _locations[_previousLocation]); //Check the distance between the camera and the previous point
-            if (distanceToPreviousPoint < 0.05) //If distance is small, go to the previous set of points
-            {
-                _nextLocation -= 1;
-                _previousLocation -= 1;
-            }
-        }
-        
-        CheckValidLocationsCounters(); //Re-check whether the index values are still valid
-        transform.position = targetPos; //Apply the position calculated
+        CheckValidLocationsCounters();
+        transform.position = newPosition;
     }
 
     private Vector3 GetCameraPosition(Vector3 target)
@@ -78,16 +77,21 @@ public class CameraFollowPath : MonoBehaviour
         Vector3 startingPos = transform.position;
         var targetPos = target;
         targetPos.z = startingPos.z; //Never change z
-        targetPos.y = Mathf.Lerp(_locations[_previousLocation].y, _locations[_nextLocation].y, CalculateCameraSpeed()); //Lerp between the y values of the previous point and the next point, depending on how far the camera is between them in a 0-1 ratio
+        targetPos.y = Mathf.Lerp(_locations[_previousLocation].y, _locations[_nextLocation].y,
+            CalculateCameraSpeed()); //Lerp between the y values of the previous point and the next point, depending on how far the camera is between them in a 0-1 ratio
         targetPos.x = playerTransform.position.x; //Use player x
         return targetPos;
     }
 
     private float CalculateCameraSpeed()
     {
-        float minX = Mathf.Min(_locations[_previousLocation].x, _locations[_nextLocation].x); //Get leftmost point position.x
-        float maxX = Mathf.Max(_locations[_previousLocation].x, _locations[_nextLocation].x); //Get rightmost point position.x
-        return (transform.position.x-minX)/(maxX-minX); //Current x and the x value of the point furthest to the right both get subtracted by the leftmost point. After that current x gets divided by the right point's x to create a 0-1 ratio
+        float minX =
+            Mathf.Min(_locations[_previousLocation].x, _locations[_nextLocation].x); //Get leftmost point position.x
+        float maxX =
+            Mathf.Max(_locations[_previousLocation].x, _locations[_nextLocation].x); //Get rightmost point position.x
+        return
+            (transform.position.x - minX) /
+            (maxX - minX); //Current x and the x value of the point furthest to the right both get subtracted by the leftmost point. After that current x gets divided by the right point's x to create a 0-1 ratio
     }
 
     private void CheckValidLocationsCounters()
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -3,16 +3,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
     [SerializeField] private float movementSpeed = 3f;
-    [SerializeField] private float jumpStrength = 4.5f;
     [SerializeField] private Animator animator;
-    
-    void Start()
-    {
-        _rigidbody2D = this.GetComponent<Rigidbody2D>();
-    }
-
     void Update()
     {
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) || !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) //If both left and right are pressed or neither are pressed
@@ -21,27 +13,20 @@ public class Movement : MonoBehaviour
             return;
         }
         
+        animator.Play("Cuphead_Walk");
+
         Vector3 localScale = transform.localScale;
         
         if (Input.GetKey(KeyCode.A)) //Move left
         {
             this.transform.position += new Vector3(-movementSpeed * Time.deltaTime, 0, 0);
             transform.localScale = new Vector3(-Math.Abs(localScale.x), localScale.y, localScale.z);
-            //animator.Play("Cuphead_Walk");
         }
         
         if (Input.GetKey(KeyCode.D)) //Move right
         {
             this.transform.position += new Vector3(movementSpeed * Time.deltaTime, 0, 0);
             transform.localScale = new Vector3(Math.Abs(localScale.x), localScale.y, localScale.z);
-            //animator.Play("Cuphead_Walk");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space)) //Jump
-        {
-            Vector3 currentVel = _rigidbody2D.velocity;
-            currentVel.y += jumpStrength;
-            _rigidbody2D.velocity = currentVel;
         }
     }
 }
